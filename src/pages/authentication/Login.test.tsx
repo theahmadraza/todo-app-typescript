9d3f6ff02a362a05fe43a5cdd6febf77a81f331e
Muhammad Ahamd Raza <60704740+theahmadraza@users.noreply.github.com>
2023-07-17 11:49:47 +0500

    Fix useNavigate issue in Test cases

--- src/pages/authentication/Login.test.tsx
+++ src/pages/authentication/Login.test.tsx
@@ -1,8 +1,16 @@
 import { render} from '@testing-library/react';
 import Login from './login';
 
+jest.mock('react-router-dom', ()=> ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate:jest.fn(),
+})
+);
+
 describe("Render Login Page", ()=>{
     it('should render Login', () => {
-        render(<Login />);
+        render(
+         <Login />
+       );
     });
 })
